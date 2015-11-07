e22379fb48810943bd7f6155f35b24c0416ec769
sgriesmer <sgriesmer@comcast.net>
2015-11-07 18:37:25 +0000

    6 tests pending

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,6 +2,10 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
   # Get a word from remote "random word" service
 
@@ -10,6 +14,30 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter.empty? 
+    raise ArgumentError if letter =~ /[^(a-zA-Z)]/
+    letter = letter.downcase
+    if @word.include? letter
+      if !@guesses.include? letter
+        @guesses << letter
+        return true
+      else
+        return false
+      end
+    else
+      if !@wrong_guesses.include? letter
+        @wrong_guesses << letter
+        return true
+      else
+        return false
+      end
+    end
   end
 
   def self.get_random_word
