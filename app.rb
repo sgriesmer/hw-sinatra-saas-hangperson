d171bb20f833007f24a6a1b5fcff70a461519f33
sgriesmer <sgriesmer@comcast.net>
2015-11-08 16:15:39 +0000

    All cucumber tests passed

--- app.rb
+++ app.rb
@@ -40,7 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    redirect '/show'
+    if @game.guess(letter)
+      redirect '/show'
+    else
+      flash[:message] = "You have already used that letter."
+      redirect '/show'
+    end
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -50,6 +55,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    end
+    if @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
@@ -62,5 +73,4 @@ class HangpersonApp < Sinatra::Base
     ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
   end
-  
 end
